d1ab6789e4874d64e9eb3e91495cd460129fd43c
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2019-02-22 14:45:57 -0800

    Update climbingStairs.java

--- easyInterviewLeetcode/climbingStairs.java
+++ easyInterviewLeetcode/climbingStairs.java
@@ -12,4 +12,24 @@ class Solution {
     }
 }
 
-// SOLUTION 2: Dynamic Programming
+// SOLUTION 2: Dynamic Programming with Memoization
+
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n == 1) return 1;
+        
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        
+        for(int i = 2; i < n; i++) {
+            
+            dp[i] = dp[i - 1] + dp[i - 2];
+            
+        }
+        
+        return dp[n - 1];
+        
+    }
+}
