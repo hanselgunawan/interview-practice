edc43ce496a30321776ea8ad82bd42f59d718dd7
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2019-02-22 01:15:07 -0800

    Update validAnagram.java

--- easyInterviewLeetcode/validAnagram.java
+++ easyInterviewLeetcode/validAnagram.java
@@ -25,3 +25,61 @@ class Solution {
         return true;
     }
 }
+
+// SOLUTION 2: HashMap
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()) return false;
+        
+        Map<Character, Integer> map1 = new HashMap<Character, Integer>();
+        Map<Character, Integer> map2 = new HashMap<Character, Integer>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            
+            if(!map1.containsKey(s.charAt(i))) {
+                
+                map1.put(s.charAt(i), 1);
+                
+            } else {
+                
+                int count = map1.get(s.charAt(i));
+                map1.put(s.charAt(i), count + 1);
+                
+            }
+            
+        }
+        
+        for(int j = 0; j < t.length(); j++) {
+            
+            if(!map2.containsKey(t.charAt(j))) {
+                
+                map2.put(t.charAt(j), 1);
+                
+            } else {
+                
+                int count = map2.get(t.charAt(j));
+                map2.put(t.charAt(j), count + 1);
+                
+            }
+            
+        }
+        
+        for(Map.Entry<Character, Integer> entry : map1.entrySet()) {
+            
+            if(map2.containsKey(entry.getKey())) {
+                
+                if(map2.get(entry.getKey()) != entry.getValue()) return false;
+                
+            } else {
+                
+                return false;
+                
+            }
+            
+        }
+        
+        return true;
+    }
+}
